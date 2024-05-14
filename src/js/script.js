0fded714e39100074d243ae9e162a8c550586355
krishna <ky680yadav@gmail.com>
2024-05-14 11:55:36 +0530

    now app background is interactive when wheel chnages its color

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ var colorPicker = new iro.ColorPicker("#picker", {
     // Set the size of the color picker
     width: 250,
     // Set the initial color to pure red
-    color: "#f00",
+    color: "#dbffc4",
     margin:15,
 
     // color wheel will not fade to black when the lightness decreases.
@@ -11,16 +11,41 @@ var colorPicker = new iro.ColorPicker("#picker", {
     borderColor:"#fff",
   });
 
-// here we put the hex value in the box 
+// here we put the hex value in the box  and changing the background as wheel changes the color
 
 const hexbox = document.getElementById("hexbox")
 // for testing purpose 
 console.log(hexbox)
 
-colorPicker.on(["color:init","color:change"],(color)=>{
-  hexbox.innerHTML = color.hexString})
 
+
+colorPicker.on(["color:init","color:change"],(color)=>{
+  hexbox.innerHTML = color.hexString
+  const divEle = document.getElementById("changecolor")
+  divEle.style.backgroundColor = color.hexString
+
+
+})
   
+
+
+
+
+
+
+
+
+
+// changing the background of app when color picker wheel change its colors
+
+// colorPicker.on("color:setActive",(color)=>{
+
+//   const divEle = document.getElementById("changecolor")
+//   divEle.style.backgroundColor = 
+
+
+// })
+
 
 
 
