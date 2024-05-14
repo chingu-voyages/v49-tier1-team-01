eb05864e4186cda74b828263b9b08e54c5c3449d
krishna <ky680yadav@gmail.com>
2024-05-14 11:41:15 +0530

    color wheel added and give hex val

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,31 @@ var colorPicker = new iro.ColorPicker("#picker", {
     // Set the initial color to pure red
     color: "#f00",
     margin:15,
+
+    // color wheel will not fade to black when the lightness decreases.
+    wheelLightness:false,
+    borderWidth:2,
+    borderColor:"#fff",
   });
+
+// here we put the hex value in the box 
+
+const hexbox = document.getElementById("hexbox")
+// for testing purpose 
+console.log(hexbox)
+
+colorPicker.on(["color:init","color:change"],(color)=>{
+  hexbox.innerHTML = color.hexString})
+
   
 
-var hex = colorPicker.color.hexString;
-console.log(hex);
-alert(hex)
+
+
+
+
+
+
+// here .on() method is from color picker library not from the vanilla js
+  // colorPicker.on('color:change', function(color) {
+  //   console.log(color.hexString);
+  // });
