7255e692fbedfd7af37b8cf5ef36b809a4bbc810
Sebin <sebinhong068@gmail.com>
2024-06-12 20:22:01 +0900

    add github logo

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,11 +4,11 @@ export default function Footer(props) {
   return (
     <footer className="text-center" style={{backgroundColor : props.backgroundColor}}>
     <p className="footer-text">
-      Repository here: {" "}<a
+      <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/chingu-voyages/v49-tier1-team-01"
-      >https://github.com/chingu-voyages/v49-tier1-team-01
+      > <img style={{width: "6vw", height: "6vw"}} src="../github-mark.png"/>
       </a>
       </p>
     </footer>
